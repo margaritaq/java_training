c4f41ad74beee130bcaa1f9da14645ef2ca303e5
mgabureac <margarita.gabureac@endava.com>
2025-01-13 13:28:19 +0200

    JT-02-OOP resolve conflicts and rebase from master
    # Conflicts:
    #	src/main/resources/task02.txt

---
+++ src/main/java/task02/Crab.java
@@ -0,0 +1,44 @@
+package task02;
+
+public class Crab extends Animal implements Walking,Running,Swimming{
+    public Crab(){
+    }
+    public Crab(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public Crab(String name, int age, String color){
+        this.name=name;
+        this.age=age;
+        this.color=color;
+    }
+    @Override
+    public void say() {
+        System.out.println("Crab can't speak ");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Crab like INVERTEBRATES");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Zzzzzzz Crab is sleeping");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Crab is running");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Crab is swimming");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Crab is walking");
+    }
+}
---
+++ src/main/java/task02/Running.java
@@ -0,0 +1,5 @@
+package task02;
+
+public interface Running {
+    abstract void run();
+}
